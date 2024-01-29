a3152965385ca4f612fac56a2e366089119c4992
sovath <sovathkeo123@gmail.com>
2024-01-29 21:03:03 +0700

    feature : add more cache operation

--- src/main/java/kh/com/cellcard/services/caching/CachingService.java
+++ src/main/java/kh/com/cellcard/services/caching/CachingService.java
@@ -30,11 +30,40 @@ public class CachingService {
         return Optional.ofNullable(redisTemplateString.opsForValue().get(key));
     }
 
+    public Optional<Object> getForHash(String key, Object hashKey){
+        if (isNullKeyOrDisconnected(key)) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(redisTemplateString.opsForHash().get(key, hashKey));
+    }
+
+    public boolean setForHash(String key, Object hashKey, Object value){
+        if (isNullKeyOrDisconnected(key)) {
+            return false;
+        }
+        redisTemplateString.opsForHash().put(key, hashKey, value);
+        return true;
+    }
+
     public void setString(String key, String value,long lifeTime, TimeUnit timeUnit) {
         if (!isConnectedToRedis() || StringHelper.isNullOrEmpty(key) || StringHelper.isNullOrEmpty(value)) {
             return;
         }
         redisTemplateString.opsForValue().set(key, value,lifeTime, timeUnit);
+    }
+
+    public Long getExpire(String key) {
+        if (isNullKeyOrDisconnected(key)) {
+            return (long) -1;
+        }
+        return redisTemplateString.getExpire(key);
+    }
+
+    public void setExpire(String key, long lifeTime, TimeUnit timeUnit) {
+        if (isNullKeyOrDisconnected(key)) {
+            return;
+        }
+        redisTemplateString.expire(key, lifeTime, timeUnit);
     }
 
     public boolean isConnectedToRedis() {
@@ -43,9 +72,13 @@ public class CachingService {
             return Objects.requireNonNull(connection.ping()).equalsIgnoreCase("PONG");
         } catch (Exception e) {
             if(e instanceof RedisConnectionFailureException) {
-                throw e;
+                e.printStackTrace();
             }
             return false;
         }
+    }
+
+    private boolean isNullKeyOrDisconnected(String key) {
+        return StringHelper.isNullOrEmpty(key) || !isConnectedToRedis();
     }
 }
